928b60df1cc522368fc3885ce6df79fab07e86a7
PrinceSharma-afk <116869578+PrinceSharma-afk@users.noreply.github.com>
2024-07-24 13:41:37 +0530

    Update BalanceOfTransferAll.js

--- scripts/BalanceOfTransferAll.js
+++ scripts/BalanceOfTransferAll.js
@@ -3,9 +3,9 @@ const { ethers } = require("hardhat");
 async function main() {
     const [deployer] = await ethers.getSigners();
     const MyNFT = await ethers.getContractFactory("MyNFT");
-    const myNFT = await MyNFT.attach("0x79cBb5F6C7B8712a32A9602BC11EBa596Dc2ad5b");
+    const myNFT = await MyNFT.attach("0x79cBb5F6C7B8712a32A9602BC11EBa596Dc2ad5b"); // contract address.
 
-    const balance = await myNFT.balanceOf("0x0ab671A61d1Ac32df09739A4E0111f5bcB921306");
+    const balance = await myNFT.balanceOf("0x0ab671A61d1Ac32df09739A4E0111f5bcB921306");// to wallet address.
     console.log(`Balance of recipient: ${balance.toString()}`);
 }
 
